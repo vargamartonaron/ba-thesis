4696902a899ecd9886dde6034c4606445e839d06
Marton Aron Varga <martonaronvarga@gmail.com>
2024-05-24 22:16:59 +0200

    update

--- h_cse_conditions_time_elapsed.R
+++ h_cse_conditions_time_elapsed.R
@@ -12,12 +12,15 @@ h_cse_conditions_time_elapsed_priors <- c(
   set_prior("normal(0, 5)", class = "b", coef = "congruencyincongruent:prev_congruencyincongruent:condition_neutral"),
   set_prior("student_t(5, 0, 10)", class = "sd"),
   set_prior("lkj(1.5)", class = "cor"),
-  set_prior("normal(0, 5)", class = "b", coef = "motime_elapsed"),
-  set_prior("normal(0, 3)", class = "b", coef = "motime_elapsed:condition_positive"),
-  set_prior("normal(0, 3)", class = "b", coef = "motime_elapsed:condition_negative"),
+  set_prior("normal(0, 5)", class = "b", coef = "time_elapsed"),
+  set_prior("normal(0, 3)", class = "b", coef = "time_elapsed:condition_positive"),
+  set_prior("normal(0, 3)", class = "b", coef = "time_elapsed:condition_negative"),
   set_prior("normal(0, 10)", class = "b")
 )
 
-h_cse_conditions_time_elapsed_formula <- bf(rt ~ congruency * prev_congruency * mo(time_elapsed) * (condition_positive + condition_negative + condition_neutral) + (1 + congruency + prev_congruency | participant_id))
+h_cse_conditions_time_elapsed_formula <- bf(rt ~ congruency * prev_congruency * time_elapsed * (condition_positive + condition_negative + condition_neutral) + (1 + congruency + prev_congruency | participant_id))
 
-h_cse_conditions_time_elapsed_fit <- brm(formula = h_cse_conditions_time_elapsed_formula, data = train_df, prior = h_cse_conditions_time_elapsed_priors, seed = 1234, sample_prior = "yes", save_model = file.path(model_dir, "h_cse_conditions_time_elapsed.stan"), file = file.path(model_dir, "h_cse_conditions_time_elapsed"), init = 0, iter = 10000, warmup = 2000, cores =  16, file_refit = "on_change")
+h_cse_conditions_time_elapsed_fit <- brm(formula = h_cse_conditions_time_elapsed_formula,
+					 data = train_df, 
+				       	prior = h_cse_conditions_time_elapsed_priors, 
+					 seed = 1234, sample_prior = "yes", save_model = file.path(model_dir, "h_cse_conditions_time_elapsed.stan"), file = file.path(model_dir, "h_cse_conditions_time_elapsed"), init = 0, iter = 10000, warmup = 2000, cores = 4, file_refit = "always")
